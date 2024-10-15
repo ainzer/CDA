01e36f33a5d5cc36693f5365edd70c9eba23a562
ainzer <tony.spicher@hotmail.com>
2024-10-15 14:08:09 +0200

    15/10/2024

---
+++ sql/base_exemple_2.sql
@@ -0,0 +1,22 @@
+33. Rechercher le prénom des employés et le numéro de la région de leur département.
+
+SELECT employe.prenom, dept.noregion FROM employe JOIN dept ON employe.nodep = dept.nodept;
+
+34. Rechercher le numéro du département, le nom du département, le nom des employés classés par numéro de département (renommer les tables utilisées).
+
+SELECT d.nodept AS "Numéro Département", d.nom AS "Nom Département", e.nom AS "Nom Employé" FROM employe e JOIN dept d ON e.nodep = d.nodept ORDER BY d.nodept;
+
+35. Rechercher le nom des employés du département Distribution.
+
+SELECT e.nom AS "Nom Enployé" FROM employe e JOIN dept d ON e.nodep = d.nodept WHERE d.nom = 'Distribution';
+
+36. Rechercher le nom et le salaire des employés qui gagnent plus que leur patron, et le nom et le salaire de leur patron.
+
+SELECT e.nom AS "Nom Employé", e.salaire AS "Salaire Employé", p.nom AS "Nom Patron", p.salaire AS "Salaire Patron" FROM employe e JOIN employe p ON e.nosup = p.noemp WHERE e.salaire > p.salaire;
+
+37. Rechercher le nom et le titre des employés qui ont le même titre que Amartakaldire.
+
+SELECT e.nom AS "Nom Employé", e.titre AS "Titre" FROM employe e WHERE e.titre = (SELECT titre FROM employe WHERE nom = 'Amartakaldire');
+
+38. Rechercher le nom, le salaire et le numéro de département des employés qui gagnent plus qu un seul employé du département 31, classés par numéro de département et salaire.
+
