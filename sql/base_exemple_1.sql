a05f380bd0a25aad4179c3079b42d47fe112cb36
ainzer <tony.spicher@hotmail.com>
2024-10-15 16:00:35 +0200

    15/10/2024

---
+++ sql/base_exemple_1.sql
@@ -0,0 +1,25 @@
+-- 1. Afficher toutes les informations concernant les employés.
+
+-- 2. Afficher toutes les informations concernant les départements.
+
+-- 3. Afficher le nom, la date d'embauche, le numéro du supérieur, le numéro de département et le salaire de tous les employés.
+
+-- 4. Afficher le titre de tous les employés.
+
+-- 5. Afficher les différentes valeurs des titres des employés.
+
+-- 6. Afficher le nom, le numéro d'employé et le numéro du département des employés dont le titre est « Secrétaire ».
+
+-- 7. Afficher le nom et le numéro de département dont le numéro de département est supérieur à 40.
+
+-- 8. Afficher le nom et le prénom des employés dont le nom est alphabétiquement antérieur au prénom.
+
+-- 9. Afficher le nom, le salaire et le numéro du département des employés dont le titre est « Représentant », le numéro de département est 35 et le salaire est supérieur à 20000.
+
+-- 10. Afficher le nom, le titre et le salaire des employés dont le titre est « Représentant » ou dont le titre est « Président ».
+
+-- 11. Afficher le nom, le titre, le numéro de département, le salaire des employés du département 34, dont le titre est « Représentant » ou « Secrétaire ».
+
+-- 12. Afficher le nom, le titre, le numéro de département, le salaire des employés dont le titre est Représentant, ou dont le titre est Secrétaire dans le département numéro 34.
+
+-- 13. Afficher le nom, et le salaire des employés dont le salaire est compris entre 20000 et 30000.
